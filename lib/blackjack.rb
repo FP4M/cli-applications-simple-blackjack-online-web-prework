c3e4ec5c9c05c1bcd2b5b9f8eb2901765cb5aed3
Prince Charles <princecharles35@yahoo.com>
2019-09-26 13:18:11 +0000

    Automatically backed up by Learn

---
+++ lib/blackjack.rb
@@ -0,0 +1,10 @@
+describe "#display_card_total" do
+  it "accepts one argument, the card total" do
+    expect { display_card_total(7) }.to_not raise_error
+  end
+ 
+  it "prints the value of the cards to the screen" do
+    expect($stdout).to receive(:puts).with("Your cards add up to 8")
+    display_card_total(8)
+  end
+  ....
